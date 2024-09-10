b7a5fa08ddf8bdc10556f311b8e59f96fee80aed
Lisa Fujita <lisafujita90@gmail.com>
2024-09-10 12:01:18 -0400

    first javascript assignment

---
+++ index.js
@@ -0,0 +1,60 @@
+// console.log('This is my first JavaScript program');
+
+// // Alert only works in the browser not in node
+// // alert('this is an alert');
+
+// const myName = 'Lisa';
+// const tax = 0.2;
+// const bool = true;
+// console.log(myName);
+// console.log(tax);
+// console.log(bool);
+
+// console.log('A starting salary for web developer in New York is $96,000');
+// console.log(96000 * tax);
+
+// let state = 'New York';
+// let salary = 80000;
+
+// console.log(
+//   'a starting salary for a web developer in' +
+//     state +
+//     'is $' +
+//     salary +
+//     'and the tax is' +
+//     salary * tax
+// );
+
+// console.log(
+//   `a starting salary for a web developer in ${state} is $${salary} and the tax is' (salary * tax)`
+// );
+
+// const five = 5;
+// const seven = 7;
+
+// console.log(five * seven);
+// console.log(five - seven);
+// console.log(five / seven);
+// console.log(five + seven);
+
+// let letter = 'l';
+// console.log(typeof five);
+// console.log(typeof letter);
+
+const ten = 5 + 5;
+const forty = 8 * 5;
+const thirtyNine = 50 - 11;
+// Should use const instead of let since these values will not change
+
+console.log(`${ten}-${forty}-${thirtyNine}`);
+alert(
+  `You have received this message because you have been chosen to open an important vault. Here is the secret combination: ${ten}-${forty}-${thirtyNine}`
+);
+
+// 1. Start by declaring variables with a const using a math equation that gives an output of 10 - 40 -39.
+// First variable ten, result of the equation will be 10
+// Second variable forty, result of the equation will be 40
+// Third variable thirtyNine, result of the equation will be 39
+// 2.The result will then be logged for debugging purposes
+// 3. Display a dialogue box to show a string and the result of the three equations
+// End Program
